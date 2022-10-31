4f30d02bb7e429ab9c64d031fdc2d358d8facfbd
4513ECHO <4513echo@gmail.com>
2022-11-01 00:28:31 +0900

    refactor: Split typing definitions

--- denops/gitter/faye/mod.ts
+++ denops/gitter/faye/mod.ts
@@ -1,64 +1,10 @@
-export enum Channel {
-  HANDSHAKE = "/meta/handshake",
-  CONNECT = "/meta/connect",
-  SUBSCRIBE = "/meta/subscribe",
-}
-
-export type Advice = {
-  reconnect: "handshake";
-} | {
-  reconnect: "retry";
-  interval: number;
-  timeout: number;
-};
-
-export interface HandshakeResponse<Ext = unknown> {
-  channel: Channel.HANDSHAKE;
-  successful: boolean;
-  version: "1.0";
-  supportedConnectionTypes: string[];
-  clientId: string;
-  advice?: Advice;
-  ext?: Ext;
-}
-
-export interface ConnectResponse {
-  channel: Channel.CONNECT;
-  clientId: string;
-  successful: boolean;
-  advice?: Advice;
-}
-
-export interface SubscribedItem<Data = unknown, Ext = unknown> {
-  channel: string;
-  data: Data;
-  id: string;
-  ext?: Ext;
-}
-
-export type RequestType<Ext = unknown> =
-  | {
-    channel: Channel.HANDSHAKE;
-    version: "1.0";
-    supportedConnectionTypes: string[];
-    minimumVersion?: string;
-    ext?: Ext;
-    id?: string;
-  }
-  | {
-    channel: Channel.CONNECT;
-    clientId: string;
-    connectionType: string;
-    ext?: Ext;
-    id?: string;
-  }
-  | {
-    channel: Channel.SUBSCRIBE;
-    clientId: string;
-    subscription: string;
-    ext?: Ext;
-    id?: string;
-  };
+import {
+  Channel,
+  type ConnectResponse,
+  type HandshakeResponse,
+  type RequestType,
+  type SubscribedItem,
+} from "./types.ts";
 
 export class Client {
   #clientId?: string;
