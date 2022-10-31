a5f9e25412a93284d8e8429711158e4e5da0f810
4513ECHO <4513echo@gmail.com>
2022-11-01 00:25:26 +0900

    feat: Improve faye typing

--- denops/gitter/faye/mod.ts
+++ denops/gitter/faye/mod.ts
@@ -12,29 +12,53 @@ export type Advice = {
   timeout: number;
 };
 
-export interface HandshakeResponse<T = unknown> {
+export interface HandshakeResponse<Ext = unknown> {
   channel: Channel.HANDSHAKE;
   successful: boolean;
   version: "1.0";
   supportedConnectionTypes: string[];
   clientId: string;
   advice?: Advice;
-  ext?: T;
+  ext?: Ext;
 }
 
 export interface ConnectResponse {
-  clientId: string;
   channel: Channel.CONNECT;
+  clientId: string;
   successful: boolean;
   advice?: Advice;
 }
 
-export interface SubscribedItem<T = unknown, U = unknown> {
+export interface SubscribedItem<Data = unknown, Ext = unknown> {
   channel: string;
-  data: T;
+  data: Data;
   id: string;
-  ext?: U;
+  ext?: Ext;
 }
+
+export type RequestType<Ext = unknown> =
+  | {
+    channel: Channel.HANDSHAKE;
+    version: "1.0";
+    supportedConnectionTypes: string[];
+    minimumVersion?: string;
+    ext?: Ext;
+    id?: string;
+  }
+  | {
+    channel: Channel.CONNECT;
+    clientId: string;
+    connectionType: string;
+    ext?: Ext;
+    id?: string;
+  }
+  | {
+    channel: Channel.SUBSCRIBE;
+    clientId: string;
+    subscription: string;
+    ext?: Ext;
+    id?: string;
+  };
 
 export class Client {
   #clientId?: string;
@@ -44,8 +68,7 @@ export class Client {
   }
 
   async #fetch(
-    channel: string,
-    additionalBody?: Record<string, unknown>,
+    body: RequestType,
   ): Promise<Response> {
     return await fetch(this.#endpoint, {
       method: "POST",
@@ -53,18 +76,14 @@ export class Client {
         "Content-Type": "application/json",
         Pragma: "no-cache",
       },
-      body: JSON.stringify({
-        channel,
-        connectionType: "long-polling",
-        ...additionalBody,
-      }),
+      body: JSON.stringify(body),
     });
   }
 
   // TODO: Add extensible feature
-  //       Make this.#fetch typing more strict (detect from Channel)
   async handshake(token: string): Promise<HandshakeResponse> {
-    const handshake = await this.#fetch(Channel.HANDSHAKE, {
+    const handshake = await this.#fetch({
+      channel: Channel.HANDSHAKE,
       version: "1.0",
       supportedConnectionTypes: ["long-polling"],
       ext: { token },
@@ -77,13 +96,16 @@ export class Client {
   async *subscribe<T = unknown, U = unknown>(
     channel: string,
   ): AsyncIterableIterator<SubscribedItem<T, U>> {
-    await this.#fetch(Channel.SUBSCRIBE, {
-      clientId: this.#clientId,
+    await this.#fetch({
+      channel: Channel.SUBSCRIBE,
+      clientId: this.#clientId!,
       subscription: channel,
     });
     while (true) {
-      const response = await this.#fetch(Channel.CONNECT, {
-        clientId: this.#clientId,
+      const response = await this.#fetch({
+        channel: Channel.CONNECT,
+        clientId: this.#clientId!,
+        connectionType: "long-polling",
       });
       const result = await response.json() as
         | [ConnectResponse]
